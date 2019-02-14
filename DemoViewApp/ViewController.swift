a988e761adc3e92856137cde470d64c6bb2141f2
ambar <chavan.amber@siliconstack.com.au>
2019-02-14 12:27:39 +0530

    init

---
+++ DemoViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoViewApp
+//
+//  Created by Macbook on 14/02/19.
+//  Copyright © 2019 SiliconStack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
